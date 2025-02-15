48672cc6fb97b288c94989665966a91369387a0b
atulaher <atul.aher1915@gmail.com>
2025-02-15 17:43:52 +0530

    User entity added

---
+++ src/main/java/com/useSvc/models/userModels/User.java
@@ -0,0 +1,11 @@
+package com.useSvc.models.userModels;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String id;
+    private String name;
+    private String emailId;
+    private String mobileNumber;
+}
